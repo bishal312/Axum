9b2453c51343075773d1f6386c2c937b7515e8d5
Bishal Kunwar Magar <bishalm626@gmail.com>
2026-07-29 17:21:07 +0545

    Add serde dependency and implement mirror_body_json route

--- hello_world/src/routes/mod.rs
+++ hello_world/src/routes/mod.rs
@@ -1,12 +1,14 @@
 mod hello_world;
 mod mirror_body;
+mod mirror_body_json;
 use axum::{Router, routing::{get, post}};
 
 use hello_world::hello_world;
 
-use crate::routes::mirror_body::mirror_body;
+use crate::routes::{mirror_body::mirror_body, mirror_body_json::mirror_body_json};
 
 pub fn create_routes() -> Router {
     Router::new().route("/", get(hello_world))
-    .route("/", post(mirror_body))
+    .route("/mirror_body", post(mirror_body))
+    .route("/mirror_body_json", post(mirror_body_json))
 }
