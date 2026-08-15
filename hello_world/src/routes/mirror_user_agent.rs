fe86ff2857cbefcf115a5e55b48dfbe19f2b29d3
Bishal Kunwar Magar <bishalm626@gmail.com>
2026-08-15 21:17:29 +0545

    Add query_params and mirror_user_agent routes, update dependencies

---
+++ hello_world/src/routes/mirror_user_agent.rs
@@ -0,0 +1,6 @@
+use axum_extra::TypedHeader;
+use headers::UserAgent;
+
+pub async fn mirror_user_agent(TypedHeader(user_agent): TypedHeader<UserAgent>) -> String {
+    user_agent.to_string()
+}
--- hello_world/src/routes/mod.rs
+++ hello_world/src/routes/mod.rs
@@ -1,19 +1,33 @@
 mod hello_world;
 mod mirror_body;
 mod mirror_body_json;
+mod mirror_user_agent;
 mod path_variables;
-use axum::{Router, routing::{get, post}};
+mod query_params;
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
 use hello_world::hello_world;
 
 use path_variables::path_variables;
 
-use crate::routes::{mirror_body::mirror_body, mirror_body_json::mirror_body_json, path_variables::hard_coded_path};
+use query_params::query_params;
+
+use crate::routes::{
+    mirror_body::mirror_body, mirror_body_json::mirror_body_json, path_variables::hard_coded_path,
+};
+
+use mirror_user_agent::mirror_user_agent;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(hello_world))
-    .route("/mirror_body", post(mirror_body))
-    .route("/mirror_body_json", post(mirror_body_json))
-    .route("/path_variables/{id}", get(path_variables))
-    .route("/path_variables/15", get(hard_coded_path))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/mirror_body", post(mirror_body))
+        .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variables/{id}", get(path_variables))
+        .route("/path_variables/15", get(hard_coded_path))
+        .route("/query_params", get(query_params))
+        .route("/mirror_user_agent", get(mirror_user_agent))
 }
